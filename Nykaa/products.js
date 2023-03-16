47a2ef99dbcfaf33cbf3dc0aa0cb47283355d14a
VivekGhalout <vivek.ghalout@gmail.com>
2023-03-17 02:20:31 +0530

    Pro Page

---
+++ Nykaa/products.js
@@ -0,0 +1,238 @@
+var products =[
+    {
+        id:1,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/1/5/15390c0NYKAC00001612_1.jpg",
+        pro_name:"Nykaa Cosmetics Serial Kisser Tinted Balm Long-lasting Combo",
+        price:"₹322",
+        strikeoffprice:"₹358",
+        off:"10% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★★",
+
+    },
+    {
+        id:2,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/0/6/06104a5NYKAC00001608_1.jpg",
+        pro_name:"Nykaa Cosmetics Glamoreyes Color Eye",
+        price:"₹798",
+        strikeoffprice:"₹998",
+        off:"20% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★",
+
+    },
+    {
+        id:3,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/6/b/6bae4a5NYKAC00001607_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Gloss Flirty Nude + So",
+        price:"₹743",
+        strikeoffprice:"₹874",
+        off:"15% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★",
+
+    },
+    {
+        id:4,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/3/0/301e90eNYKAC00001599_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Tattoo Liquid Lipstick Set",
+        price:"₹1528",
+        strikeoffprice:"₹1798",
+        off:"15% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★",
+
+    },
+    {
+        id:5,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/e/1/e1e890eNYKAC00001596_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Tattoo Liquid Lipstick Set",
+        price:"₹1528",
+        strikeoffprice:"₹1798",
+        off:"20% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★",
+
+    },
+    {
+        id:6,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/1/5/15390c0NYKAC00001612_1.jpg",
+        pro_name:"Nykaa Cosmetics Serial Kisser Tinted Balm Long-lasting Combo",
+        price:"₹322",
+        strikeoffprice:"₹358",
+        off:"10% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★★",
+
+    },
+    {
+        id:7,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/0/6/06104a5NYKAC00001608_1.jpg",
+        pro_name:"Nykaa Cosmetics Glamoreyes Color Eye",
+        price:"₹798",
+        strikeoffprice:"₹998",
+        off:"20% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★",
+
+    },
+    {
+        id:8,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/6/b/6bae4a5NYKAC00001607_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Gloss Flirty Nude + So",
+        price:"₹743",
+        strikeoffprice:"₹874",
+        off:"15% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★",
+
+    },
+    {
+        id:9,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/3/0/301e90eNYKAC00001599_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Tattoo Liquid Lipstick Set",
+        price:"₹1528",
+        strikeoffprice:"₹1798",
+        off:"15% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★",
+
+    },
+    {
+        id:10,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/e/1/e1e890eNYKAC00001596_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Tattoo Liquid Lipstick Set",
+        price:"₹1528",
+        strikeoffprice:"₹1798",
+        off:"20% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★",
+
+    },
+    {
+        id:11,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/1/5/15390c0NYKAC00001612_1.jpg",
+        pro_name:"Nykaa Cosmetics Serial Kisser Tinted Balm Long-lasting Combo",
+        price:"₹322",
+        strikeoffprice:"₹358",
+        off:"10% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★★",
+
+    },
+    {
+        id:12,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/0/6/06104a5NYKAC00001608_1.jpg",
+        pro_name:"Nykaa Cosmetics Glamoreyes Color Eye",
+        price:"₹798",
+        strikeoffprice:"₹998",
+        off:"20% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★",
+
+    },
+    {
+        id:13,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/6/b/6bae4a5NYKAC00001607_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Gloss Flirty Nude + So",
+        price:"₹743",
+        strikeoffprice:"₹874",
+        off:"15% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★",
+
+    },
+    {
+        id:14,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/3/0/301e90eNYKAC00001599_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Tattoo Liquid Lipstick Set",
+        price:"₹1528",
+        strikeoffprice:"₹1798",
+        off:"15% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★",
+
+    },
+    {
+        id:15,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/e/1/e1e890eNYKAC00001596_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Tattoo Liquid Lipstick Set",
+        price:"₹1528",
+        strikeoffprice:"₹1798",
+        off:"20% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★",
+
+    },
+    {
+        id:16,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/1/5/15390c0NYKAC00001612_1.jpg",
+        pro_name:"Nykaa Cosmetics Serial Kisser Tinted Balm Long-lasting Combo",
+        price:"₹322",
+        strikeoffprice:"₹358",
+        off:"10% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★★",
+
+    },
+    {
+        id:17,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/0/6/06104a5NYKAC00001608_1.jpg",
+        pro_name:"Nykaa Cosmetics Glamoreyes Color Eye",
+        price:"₹798",
+        strikeoffprice:"₹998",
+        off:"20% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★",
+
+    },
+    {
+        id:18,
+        quantity: 1,
+        image_url: "https://images-static.nykaa.com/media/catalog/product/tr:w-220,h-220,cm-pad_resize/6/b/6bae4a5NYKAC00001607_1.jpg",
+        pro_name:"Nykaa Cosmetics Matte Gloss Flirty Nude + So",
+        price:"₹743",
+        strikeoffprice:"₹874",
+        off:"15% Off",
+        messege:"Enjoy Free Gift",
+        rating:"★★★★",
+
+    }
+
+]
+
+displayproducts();
+
+function displayproducts() {
+    products.map(function(ele, index){
+
+        var card = document.createElement("div");
+        card.setAttribute("id", "card");
+
+        var image = document.createElement("img");
+        image.setAttribute("src", ele.image_url);
+
+        var pro_name = document.createElement("p");
+        pro_name.textContent = ele.pro_name;
+
+        
+
+    })
+}
